20f27f63486f0e45bb3dd2c47795c4375f76f824
Matthias Steiner <m@kjpjm.de>
2020-12-27 21:20:58 +0100

    add additional information

--- src/02_types/index.ts
+++ src/02_types/index.ts
@@ -1,6 +1,7 @@
 /*
  * 1.
- * export and create a variable of type boolean with the value "true".
+ * export and create a variable of type boolean with the name myBoolean and the
+ *  value "true".
  */
 
 //not implemented yet!
