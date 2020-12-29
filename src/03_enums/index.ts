a7d617f0c7686aec4c4c8c407b671665edad40f3
Matthias Steiner <m@kjpjm.de>
2020-12-29 22:48:07 +0100

    add missing varName

--- src/03_enums/index.ts
+++ src/03_enums/index.ts
@@ -1,7 +1,7 @@
 /*
 
 
-write and export an enum with the following values 
+write and export an enum with the Name myEnum and the following values 
 RED returns the string "#ff0000 
 GREEN gives the string "#00ff00".
 BLUE returns the string "#0000ff
